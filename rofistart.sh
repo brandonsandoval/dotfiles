f144219a0751c82b54447f7e8e99d9c733539afa
Brandon Sandoval <brandon.j.sandoval@outlook.com>
2017-07-24 23:43:49 +1000

    new layout 24/07/17 - blue ocean

---
+++ rofistart.sh
@@ -0,0 +1,1 @@
+rofi -show run -modi run -lines 7 -bw 0 -hide-scrollbar -color-window "argb:d817273d, argb:0ffffff, #0a111d" -color-normal "argb:0fdf6e3, #b2c1bc, argb:04b6871, #0a111d, #b2c1bc" -color-active "#fdf6e3, #268bd2, #eee8d5, #268bd2, #fdf6e3" -color-urgent "#fdf6e3, #dc322f, #eee8d5, #dc322f, #fdf6e3"
